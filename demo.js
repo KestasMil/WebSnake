eb5719f6be6a3b3316b3720b9aa66e5b3b3109b2
Kestutis Milius <kestutismilius@yahoo.com>
2019-04-11 08:23:31 +0100

    Improved demo

--- demo.js
+++ demo.js
@@ -1,5 +1,15 @@
 webSnake.InitGame('.snake-canvas', {
-  segSize: 10,
-  speed: 100
+  segSize: 15,
+  speed: 80
 });
 webSnake.RestartGame();
+
+document.querySelector('#mySpeedRange').oninput = function() {
+  webSnake.SetGameSpeed(this.value);
+  document.querySelector('#speed').textContent = this.value;
+}
+
+document.querySelector('#mySegSize').oninput = function() {
+  webSnake.SetSegmentSize(this.value);
+  document.querySelector('#seg-Size').textContent = this.value;
+}
