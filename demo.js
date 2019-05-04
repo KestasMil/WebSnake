e79686df86bc2e25ef8cc30c724ec9f7ae0bed88
Kestutis Milius <kestutismilius@yahoo.com>
2019-05-04 18:35:51 +0100

    Demo updated to support AutoPlay

--- demo.js
+++ demo.js
@@ -5,6 +5,10 @@ webSnake.InitGame('.snake-canvas', {
   foodClassName: "snake-food"
 });
 webSnake.RestartGame();
+webSnake.OnScoreChanged(score => {
+  document.querySelector("#score").textContent = score.CurrentScore;
+  document.querySelector("#high-score").textContent = score.HighScore;
+});
 
 document.querySelector('#mySpeedRange').oninput = function() {
   webSnake.SetGameSpeed(900 / this.value);
@@ -15,3 +19,21 @@ document.querySelector('#mySegSize').oninput = function() {
   webSnake.SetSegmentSize(this.value);
   document.querySelector('#seg-Size').textContent = this.value;
 }
+
+document.querySelector("#toggle-autoplay").addEventListener("click", e => {
+  ToggleAutoplay();
+});
+
+function ToggleAutoplay() {
+  ToggleAutoplay.on = !ToggleAutoplay.on;
+  if (ToggleAutoplay.on) {
+    document.querySelector("#toggle-autoplay").className = "snake-btn-on";
+    document.querySelector("#toggle-autoplay").textContent = "AutoPlay ON";
+  } else {
+    document.querySelector("#toggle-autoplay").textContent = "AutoPlay OFF";
+    document.querySelector("#toggle-autoplay").className = "snake-btn-off";
+  }
+  webSnake.ToggleAutoPilot();
+}
+
+ToggleAutoplay.on = false;
